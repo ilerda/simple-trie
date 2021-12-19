1a07b7cd8525f0def8501972fbea9c65fd318a1c
Alex Roberts <alecroberts92@gmail.com>
2021-12-19 14:26:18 +0000

    Add Trie Class with basic skeleton of public methods
    This contains the Trie class with the skeleton structure of public
    methods we want to implement:
    - insert
    - has_prefix
    - has_word
    - delete

---
+++ trie.py
@@ -0,0 +1,18 @@
+from node import Node
+
+class Trie(object):
+
+   def __init__(self):
+      pass
+
+   def insert(self, word):
+      pass
+
+   def has_prefix(self, prefix):
+      pass
+
+   def has_word(self, word):
+      pass
+
+   def delete(self, word):
+      pass
