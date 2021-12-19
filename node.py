ff675c720ec1581aff34f82ab94ad58c63acfbe3
Alex Roberts <alecroberts92@gmail.com>
2021-12-19 13:45:51 +0000

    Add Node Class
    This is the basic building block for the Trie implmentation. Each node
    contains a map (dict) of characters that point to the relevant next
    node.

---
+++ node.py
@@ -0,0 +1,24 @@
+class Node(object):
+
+   def __init__(self):
+      self.charMap = dict()
+      self.isWord = False
+
+   def is_word(self):
+      return self.isWord
+
+   def set_is_word(self, is_word):
+      self.isWord = is_word
+
+   def contains(self, c):
+      return c in self.charMap
+
+   def add_node(self, c):
+      if c not in self.charMap:
+         self.charMap[c] = Node()
+
+   def get_node(self, c):
+      return self.charMap.get(c)
+
+   def get_map(self):
+      return self.charMap
